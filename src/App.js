313623f222ea027c74dc43124adf8e5d36034d52
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2020-06-15 23:11:56 +0200

    add changes in files

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Nauka react
+            Nauka react i tworzenie stron
           </a>
         </header>
       </div>
