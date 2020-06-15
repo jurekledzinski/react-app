560e6b0d0ef4b303001068206daadaa943608ddb
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2020-06-15 23:05:28 +0200

    Updates

---
+++ precache-manifest.d90a24e103a9329695740090ceb96412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "609945946e82733f5686",
+    "url": "/react-app/static/css/main.881da295.chunk.css"
+  },
+  {
+    "revision": "609945946e82733f5686",
+    "url": "/react-app/static/js/main.330db106.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/react-app/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a6011feec2b1b1e43f744167dab16c29",
+    "url": "/react-app/index.html"
+  }
+];
